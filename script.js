6cb1b44012dce9346538006c942abcebaae61854
grgallosa <gr.gallosa@gmail.com>
2025-12-20 22:02:45 +0800

    update-blogs

--- script.js
+++ script.js
@@ -13,3 +13,21 @@
     document.body.classList.remove("menu-open");
     openBtn.setAttribute("aria-expanded", "false");
   });
+  
+  //scroll up button
+  const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollTopBtn.style.display = "flex";
+    } else {
+      scrollTopBtn.style.display = "none";
+    }
+  });
+
+  scrollTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
